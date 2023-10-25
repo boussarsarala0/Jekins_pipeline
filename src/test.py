30fdc88196f91f164840b97ad6e32bc51f986866
khalilsellamii <khalilsellami804@gmail.com>
2023-10-25 01:26:04 +0100

    git_hooks

--- src/test.py
+++ src/test.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env      python3
 
-
-
-
 print("This is a test file so we can include some unit testing functionalities in here")
+
+print("*")
+print("**")
+print("***")
+print("****")
+
+print ("This is testing for github hooks")
